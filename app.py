d59ae0f7bf0b9cfd32260c8431e6b802b7830581
mark <it.emc2015@gmail.com>
2026-02-16 15:43:02 +0800

    prepare main html

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+from flask import Flask,render_template
 import requests
 
-url = "https://api.api-ninjas.com/v1/zipcode?zip=90210"
-headers = {"X-Api-Key":"ncD8IQIlBbjtBhAw7m8WiJNBwXBmtqCVY0CXggIw"}
+app = Flask(__name__)
 
-result = requests.get(url,headers=headers)
+@app.route('/',methods=['GET'])
+def home():
 
-print(result.json())
-print(result.status_code)
+    url = "https://v6.exchangerate-api.com/v6/ffdd5bbcb02e1b65d7f798ac/latest/USD"
+    headers = {"api_key":"ffdd5bbcb02e1b65d7f798ac"}
+    response = requests.get(url,headers=headers)
+    rates = response.json()
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
